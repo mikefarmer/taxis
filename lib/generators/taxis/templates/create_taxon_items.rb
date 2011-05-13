02d80f2c97275079be9a0d50e570df06a4daf1b6
Mike Farmer <mfarmer@endpoint.com>
2011-05-12 20:52:45 -0600

    added indexes to taxon_items

--- lib/generators/taxis/templates/create_taxon_items.rb
+++ lib/generators/taxis/templates/create_taxon_items.rb
@@ -4,6 +4,9 @@ class CreateTaxonItems < ActiveRecord::Migration
       t.integer :taxon_id, :taxonable_id
       t.string :taxonable_type
     end
+    add_index :taxon_items, :taxon_id
+    add_index :taxon_items, :taxonable_id
+    add_index :taxon_items, :taxonable_type
   end
 
   def self.down
